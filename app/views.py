4c47ebce8f09e00888a3eb1277e10f465869c99d
Jeev <jeevjoelmanjila@gmail.com>
2025-03-21 15:07:18 +0530

    Slight edits

--- app/views.py
+++ app/views.py
@@ -2,9 +2,8 @@ from django.shortcuts import render, redirect
 
 # Index page
 def index(request):
-    if request.method == "POST":
-        redirect("app:edit_view")
     return render(request,"app/index.html")
 
+# Edit page
 def edit_view(request):
     return render(request,"app/edit.html")
